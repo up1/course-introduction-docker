418b29cb73f5f984ae122e1a2f9f9c3077f825c5
somkiat <somkiat.p@gmail.com>
2017-03-24 16:48:04 +0700

    Code to connect mysql database

--- demo/programming/connecting_container/web/index.php
+++ demo/programming/connecting_container/web/index.php
@@ -1,3 +1,11 @@
 <?php
-echo phpinfo();
+try {
+    $user = "root";
+    $password = "123456";
+    $dbh = new PDO('mysql:host=mydb;dbname=demo', $user, $password);
+    print "Create database connection successfully.";
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+}
 ?>
