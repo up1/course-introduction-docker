e88c7c7431d690d14ef7f116911deb904647733c
somkiat <somkiat.p@gmail.com>
2017-03-27 09:05:23 +0700

    Working with python

---
+++ demo/demo_dockerfile/python/webapp/app.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    provider = str(os.environ.get('PROVIDER', 'world'))
+    return '3Hello '+provider+'!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
